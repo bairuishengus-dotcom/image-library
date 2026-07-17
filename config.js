7aebbe442355efade33978aba5d19a817a9b1913
bairuishengus-dotcom <bairuishengus@gmail.com>
2026-07-17 15:31:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.IMAGE_LIBRARY_CONFIG = {
   siteTitle: "Company Image Library",
   bucket: "hhhhhr-1454825981",
   region: "ap-guangzhou",
-  manifestFile: "index.json"
+  apiUrl: "https://1454825981-7uh46obinv.ap-guangzhou.tencentscf.com"
 };
